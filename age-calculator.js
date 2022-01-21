6db0339d39971efa085012bf2f7ee165787843d2
Ryan Williams <r.d.williams.ca@gmail.com>
2022-01-21 07:38:37 +0000

    Age calculator challenge

---
+++ age-calculator.js
@@ -0,0 +1,8 @@
+function ageCalculator(name, yearOfBirth, currentYear) {
+  let age = currentYear - yearOfBirth;
+  let message = name + " is " + age + " years old."
+  return message;
+}
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
